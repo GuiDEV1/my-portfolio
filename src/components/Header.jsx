246dfa709800b482871dba735b47ee568a48d222
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-05-03 18:07:25 -0300

    1.0.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 function Header () {
-    return(
+    return (
         <header className="header">
             <div className="container">
                 <div className="row">
                     <div className="col-6">
                         <div className="header_content">
                             <div className="header-section">
-                                <h2>console.log("hello world")</h2>
-                                <h1>Guilherme Mascarenhas</h1>
-                                <p>duis aute dolor on reprehender in voluptareu <br/>dolore nulla periatur</p>
+                                <h2>Printf("hellow world!");</h2>
+                                <h1>
+                                    Guilherme Mascarenhas
+                                </h1>
+                                <p>duis aute dolor on reprehender in voluptareu dolore nulla periatur</p>
                                 <div className="header_buttons">
                                     <a href="header" className="btn btn-outiline">
                                       contrate-me
