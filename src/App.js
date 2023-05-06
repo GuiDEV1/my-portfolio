141590a5cd97c82e8eeb4496918051f34d36c0d6
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-05-06 02:43:25 -0300

    1.0.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
+      <Portfolio />
     </div>
   );
 }
--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,65 +1,67 @@
 const data = [
     {
       id: 1,
-      cover: "./images/",
+      cover: "./images/thunbPiano.jpg",
       title: "Piano em JS",
       category: "Developer", 
     } ,
     {
         id: 2,
-        cover: "./images/",
-        title: "Jogo da memoria em js",
+        cover: "./images/thunbJogoDaMemoria.png",
+        title: "Jogo da memoria",
         category: "Developer", 
       } ,
       {
         id: 3,
-        cover: "./images/",
+        cover: "./images/ThunbHopeOfChaos.png",
         title: "Hope of chaos",
         category: "Developer", 
       } ,
       {
         id: 4,
-        cover: "./images/",
+        cover: "./images/thunbAmiMais.jpg",
         title: "Ami Mais",
         category: "Developer", 
       } ,
       {
         id: 5,
-        cover: "./images/",
+        cover: "./images/thunbPokedex.png",
         title: "Pokedex",
         category: "Developer", 
       } ,
       {
         id: 6,
-        cover: "./images/",
-        title: "Tela de login League of legends",
+        cover: "./images/thunbLol.png",
+        title: "Tela de login Lol",
         category: "Developer", 
       } ,
       {
         id: 7,
-        cover: "./images/",
+        cover: "./images/thunbBfCrypto.png",
         title: "Bf Crypto",
         category: "Developer", 
       } ,
       {
         id: 8,
-        cover: "./images/",
+        cover: "./images/thunbMeuAlbum.png",
         title: "Minha playlist",
         category: "Design", 
       } ,
       {
         id: 9,
-        cover: "./images/",
+        cover: "./images/thunbHaktagro.png",
         title: "Hackatrago",
         category: "Design", 
       } ,
       {
         id: 10,
-        cover: "./images/",
+        cover: "./images/ThunbDkGamesSmaill.png",
         title: "Dk games",
         category: "Design", 
       } ,
 ];
+
+export default data;
 
 
 // Devoloper: 7 Design : 3
